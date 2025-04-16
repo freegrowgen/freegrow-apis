e406000b491ee447402bb4095940315b6a884173
SHIAK AFRID <afridayan01@gmail.com>
2025-04-16 09:39:28 +0000

    Add HomeController for handling root API requests

---
+++ src/main/java/com/freegrownextgen/freegrow/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.freegrownextgen.freegrow.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController{
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to FreeGrow NextGen API";
+    }
+
+
+
+}
