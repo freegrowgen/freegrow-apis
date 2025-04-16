00c573f5d617ff423952c34f5772bb725964d580
SHIAK AFRID <afridayan01@gmail.com>
2025-04-16 10:49:13 +0000

    Implement OTP verification in sign-up process and add TempAuthRepository for temporary user storage

---
+++ src/main/java/com/freegrownextgen/freegrow/models/appuser/TempAppUserModel.java
@@ -0,0 +1,17 @@
+package com.freegrownextgen.freegrow.models.appuser;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "temp_app_users")
+public class TempAppUserModel {
+    private String emailId;
+    private Integer otp;
+
+}
