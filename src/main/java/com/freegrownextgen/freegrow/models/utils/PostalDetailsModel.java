d460b1baecb764aad397123faa75f3fb567d2a09
SHIAK AFRID <afridayan01@gmail.com>
2025-05-02 09:24:07 +0000

    Added postal details model

---
+++ src/main/java/com/freegrownextgen/freegrow/models/utils/PostalDetailsModel.java
@@ -0,0 +1,21 @@
+package com.freegrownextgen.freegrow.models.utils;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "postal_data")
+public class PostalDetailsModel {
+    private String _id;
+    private String stateName;
+    private String districtName;
+    private Integer pincode;
+    private List<PostalPostOfficeModel> postOffice;
+
+}
